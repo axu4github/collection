c1c64853baeedf1a46f4d35aaa36c2424bb7a78e
axu4github <axu.home@gmail.com>
2017-10-11 23:48:41 +0800

    添加output文件

--- bin/start.py
+++ bin/start.py
@@ -7,19 +7,21 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-COMMAND_PATTERN = "cd {BASE_DIR}; scrapy crawl {spider_name}"
+COMMAND_PATTERN = "cd {BASE_DIR}; scrapy crawl {spider_name} -o {output}"
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+DEFAULT_OUTPUT = "{BASE_DIR}/output.csv".format(BASE_DIR=BASE_DIR)
 
 
 @click.command()
 @click.option("--spider_name", default=None, help="爬虫名称")
-def main(spider_name):
+@click.option("--output", default=DEFAULT_OUTPUT, help="爬虫名称")
+def main(spider_name, output):
     """ 启动爬虫脚本 """
     if spider_name is None:
         pass
 
     command = COMMAND_PATTERN.format(
-        BASE_DIR=BASE_DIR, spider_name=spider_name)
+        BASE_DIR=BASE_DIR, spider_name=spider_name, output=output)
     # (status, output) = commands.getstatusoutput(command)
 
     # click.echo(output)
